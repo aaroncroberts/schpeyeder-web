a099cc52e15f94d4754e055629c2ae68ef8f44ca
Aaron Roberts <roberts.348@gmail.com>
2014-03-28 09:41:57 -0400

    Continued work on expanding the grid directive.

--- src/app/components/directives/grid.js
+++ src/app/components/directives/grid.js
@@ -9,16 +9,39 @@ goog.scope(function(){
 	 * @constructor
 	 */
 	schpeyeder.web.components.directives.grid = function() {
+		
+		/**
+		 * @export
+		 *  
+		 */
+		var getProperties = function(obj) {
+			var props = [];
+			
+			for( key in obj ){
+				if(obj.hasOwnProperty(key)){
+					props.push(new { key: key, value: obj[key] });
+					
+					// recurse
+					props.push(getPropertyNames(obj[key]));
+				}
+			}
+			
+			return props;
+		};
+		
 		return{
 			restrict: 'E',
 			templateUrl: '/app/components/directives/grid.template.html',    					
 			scope: 
-			{
-			  gridData : '=',
-			  gridColumns : '='
+			{		
+				output : '=',
+				gridData : '=',
+				gridColumns : '='
 			},
 			link : function(scope, elem, attrs){				  			 
-	
+				// Get the scope data
+				var columns = scope.gridColumns;
+				var output = scope.gridData;
 			}
 		}
 		
